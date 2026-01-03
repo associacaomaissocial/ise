9113bb1a6aa88cb78c81f474c04dc6ade70bc1ff
associacaomaissocial <associacaomaissocial@gmail.com>
2026-01-03 19:51:48 +0000

    Tryas

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-document.body.insertAdjacentHTML("beforeend", '<div style="position:fixed;top:10px;left:10px;z-index:99999;background:#fff;border:1px solid #ddd;padding:6px 8px;border-radius:8px;font-size:12px">app.js carregou ✅</div>');
+const mapDiv = document.getElementById("map");
+mapDiv.style.outline = "4px solid red";
+mapDiv.insertAdjacentHTML("afterbegin", "<div style='padding:10px;font-size:14px'>#map existe ✅</div>");
+
+console.log("mapDiv height:", mapDiv.offsetHeight);
+document.body.insertAdjacentHTML(
+  "beforeend",
+  `<div style="position:fixed;top:40px;left:10px;z-index:99999;background:#fff;border:1px solid #ddd;padding:6px 8px;border-radius:8px;font-size:12px">
+     altura #map: ${mapDiv.offsetHeight}px
+   </div>`
+);
